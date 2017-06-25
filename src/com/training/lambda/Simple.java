410650c91d2052de221c6cc7651a0684f48e5d6a
cj96248 <253037585@qq.com>
2017-06-25 11:51:32 +0800

    Add lambda

--- src/com/training/lambda/Simple.java
+++ src/com/training/lambda/Simple.java
@@ -1,5 +1,20 @@
 package com.training.lambda;
 
-public class Simple {
+import java.util.Comparator;
 
+public class Simple {
+	public void testComparator(){
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer x, Integer y) {
+				return Integer.compare(x, y);
+			}
+		};
+	}
+	
+	public void testComparatorLam(){
+		Comparator<Integer> comparator = (x, y) -> Integer.compare(x, y);
+		
+	}
+	
 }
